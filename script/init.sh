454ba01665a56af01993bf8a369019439a411fc8
DeltaDev <votinovilya174@gmail.com>
2025-12-01 18:05:01 +0500

    Update init.sh

--- script/init.sh
+++ script/init.sh
@@ -6,7 +6,7 @@ $STATUS_OS
 echo "✓status: version=v.0.0.0.9-a $STATUS_VERSION"
 echo "✓status: booting=true $STATUS_BOOTING"
 echo "✓status: OS=OpenDelta Linux $STATUS_OS"
-gcc term.c commands.c files.c simple_comms.c -o term
+clang term.c commands.c files.c dltsh_base.c simple_comms.c -o term
 ./term
 chmod +x ~/os/term
 echo "✓ending os"
