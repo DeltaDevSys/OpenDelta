255838b60ace765f9443e7f6e13fa41b4a90cf77
DeltaDev <votinovilya174@gmail.com>
2025-12-13 21:13:13 +0500

    Update init.sh

--- script/init.sh
+++ script/init.sh
@@ -1,12 +1,12 @@
-cd ~/OpenDelta/sub-system
+cd ~/OpenDelta/shell/
 echo "✓starting OS..."
 $STATUS_BOOTING
 $STATUS_VERSION
 $STATUS_OS
-echo "✓status: version=v.0.0.0.9-a $STATUS_VERSION"
+echo "✓status: version=v.0.0.0.12-c $STATUS_VERSION"
 echo "✓status: booting=true $STATUS_BOOTING"
 echo "✓status: OS=OpenDelta Linux $STATUS_OS"
 clang term.c commands.c files.c dltsh_base.c simple_comms.c -o term
 ./term
-chmod +x ~/os/term
+chmod +x ./term
 echo "✓ending os"
